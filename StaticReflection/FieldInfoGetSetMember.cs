964dede201807d6e82fc038336cc97cd43ca4766
mtranter <mark.a.tranter@gmail.com>
2013-06-22 16:53:50 +0100

    Reinstate lost files...???

---
+++ StaticReflection/FieldInfoGetSetMember.cs
@@ -0,0 +1,40 @@
+﻿namespace StaticReflection
+{
+    using System;
+    using System.Reflection;
+
+    public class FieldInfoGetSetMember : ReflectedMember, IGetSetMember
+    {
+        private readonly FieldInfo _fieldInfo;
+
+        public FieldInfoGetSetMember(FieldInfo fieldInfo)
+            : base(fieldInfo)
+        {
+            _fieldInfo = fieldInfo;
+        }
+
+        public override Type MemberType
+        {
+            get { return _fieldInfo.FieldType; }
+        }
+
+        #region IGetSetMember Members
+
+        public bool IsReadonly
+        {
+            get { return false; }
+        }
+
+        public object GetValue(object instance)
+        {
+            return _fieldInfo.GetValue(instance);
+        }
+
+        public void SetValue(object instance, object value)
+        {
+            _fieldInfo.SetValue(instance, value);
+        }
+
+        #endregion
+    }
+}
---
+++ StaticReflection/PropertyInfoGetSetMember.cs
@@ -0,0 +1,40 @@
+﻿﻿namespace StaticReflection
+{
+    using System;
+    using System.Reflection;
+
+    public class PropertyInfoGetSetMember : ReflectedMember,  IGetSetMember
+    {
+        private readonly PropertyInfo _propertyInfo;
+
+        public PropertyInfoGetSetMember(PropertyInfo propertyInfo)
+            : base(propertyInfo)
+        {
+            _propertyInfo = propertyInfo;
+        }
+
+        public override Type MemberType
+        {
+            get { return _propertyInfo.PropertyType; }
+        }
+
+        #region IDeafMember Members
+
+        public bool IsReadonly
+        {
+            get { return !_propertyInfo.CanWrite; }
+        }
+
+        public object GetValue(object instance)
+        {
+            return _propertyInfo.GetValue(instance);
+        }
+
+        public void SetValue(object instance, object value)
+        {
+            _propertyInfo.SetValue(instance, value);
+        }
+
+        #endregion
+    }
+}
